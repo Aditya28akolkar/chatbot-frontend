2f07f7b6c7fc945ce12b2ed9991d091db81aa819
Aditya <adityaakolkar28@gmail.com>
2026-06-03 17:24:32 +0530

    Connect admin frontend to HF backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,25 +1,7 @@
-// // import axios from "axios";
-
-// // const api = axios.create({
-
-// //   baseURL:
-// //     "https://graspcorn-chatbot.hf.space"
-
-// // });
-
-// // export default api;
-
-
-// const api = axios.create({
-//   baseURL:
-//     "http://127.0.0.1:8001",
-// });
-
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8001",
+  baseURL: "https://graspcorn-chatbot.hf.space",
 });
 
 export default api;
